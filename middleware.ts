45476412fe267e8a079222e15173c74bc05970dd
AllanK615 <kisemboallan69@gmail.com>
2025-12-21 17:51:23 +0300

    Implemented Clerk

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Public routes: sign-in and sign-up (allow any subpaths)
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
@@ -11,9 +10,8 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // 1. Skip Next.js internals and all static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // 2. FIXED: Added the leading / here
+
     "/(api|trpc)(.*)",
   ],
 };
